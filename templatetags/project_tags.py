e221fabb1166016976460cfbe5ee4a85d9afd35b
Tobi DEGNON <tobidegnon@proton.me>
2023-11-05 23:54:41 +0100

    fix: featured value was always giving False

--- templatetags/project_tags.py
+++ templatetags/project_tags.py
@@ -13,7 +13,6 @@ def get_projects() -> list[dict[str, str]]:
     projects = get_data()["projects"]
     for project in projects:
         project["last_updated"] = parse(project["last_updated"])
-        project["featured"] = project["featured"] == "true"
 
     projects.sort(key=lambda p: p.get("last_updated"), reverse=True)
     projects.sort(key=lambda p: p.get("featured"), reverse=True)
