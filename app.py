d228784b756660373bf4fe08a100cd9e81673138
Tobi DEGNON <tobidegnon@proton.me>
2023-11-07 13:08:27 +0100

    override coltrane build command to use my custom feed

--- app.py
+++ app.py
@@ -2,10 +2,9 @@
 
 from django.core.management import execute_from_command_line
 
-from coltrane import initialize
+from coltrane import initialize, DEFAULT_INSTALLED_APPS
 
-
-wsgi = initialize(**{"ROOT_URLCONF": "pw.urls"})
+wsgi = initialize(**{"ROOT_URLCONF": "pw.urls", "INSTALLED_APPS": ["pw"] + DEFAULT_INSTALLED_APPS})
 
 if __name__ == "__main__":
     execute_from_command_line()
