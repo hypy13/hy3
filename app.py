0ee151453b50d9ec4d748884488e41e0b702fd7b
Tobi DEGNON <tobidegnon@proton.me>
2023-12-16 10:39:00 +0100

    add untools

--- app.py
+++ app.py
@@ -2,9 +2,9 @@
 
 from django.core.management import execute_from_command_line
 
-from coltrane import initialize, DEFAULT_INSTALLED_APPS
+from coltrane import initialize
 
-wsgi = initialize(**{"ROOT_URLCONF": "pw.urls", "INSTALLED_APPS": ["pw"] + DEFAULT_INSTALLED_APPS})
+wsgi = initialize(**{"ROOT_URLCONF": "pw.urls", "INSTALLED_APPS": ["pw"]})
 
 if __name__ == "__main__":
     execute_from_command_line()
